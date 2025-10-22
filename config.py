fef1b64160146833e5456f5f4fb45aeafe7fe1fe
PedronXW <pedroalmeidan@gmail.com>
2025-10-22 23:26:24 +0000

    feat

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-YOLO_MODEL_PATH = './datasets/runs/detect/train3/weights/best.pt'
+YOLO_MODEL_PATH = 'yolov8n.pt'
 REAL_ESRGAN_MODEL_PATH = 'models/realesrgan_x4plus.pth'
 DEEPSORT_MODEL_PATH = 'models/deep_sort.pb'
 
 USE_SUPERRES = True
 USE_TRACKING = True
-VIDEO_SOURCE = "./teste-2.mp4"
+VIDEO_SOURCE = "./teste.mp4"
 OCR_LANG = 'en'
